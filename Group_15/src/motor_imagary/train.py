71bff915a52a05c7c5249aaf2a1fc0d6e176f968
Niklas <niklas.kjolbro@live.com>
2026-01-16 18:33:40 +0100

    Initial setup

---
+++ Group_15/src/motor_imagary/train.py
@@ -0,0 +1,10 @@
+from motor_imagary.model import Model
+from motor_imagary.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
